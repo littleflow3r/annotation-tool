68be8b3f099e83db9d61ca65bc5b7ceb7f5f9653
littleflow3r <36025078+littleflow3r@users.noreply.github.com>
2019-02-27 16:20:10 +0900

    Update maketemplate2.py

--- maketemplate2.py
+++ maketemplate2.py
@@ -1,10 +1,13 @@
+from clean import cleantext
+import glob, os
+
 def split_sentences(text):
     sentences = text.split('.')
     sentences = [i.strip() for i in sentences]
     sentences = [i for i in sentences if len(i) > 0 ]
     return sentences
 
-def template_head(taskid, docid, anid):
+def template_head(taskid, docid, anid, fname):
     text = ''' 
     <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
     <meta charset="utf-8">
@@ -20,16 +23,17 @@ def template_head(taskid, docid, anid):
         <table border="1">
           <input type="hidden" id="taskid" value="%s" disabled>
           <tr><b>DOC ID: </b> <input type="text" id="docid" value="%s" disabled> </tr> <br>
-          <tr><b>YOUR ID: </b> <input type="text" id="anid" value="%s" ></tr>
-          <br><br>
-      ''' % (taskid, docid, anid)
+          <tr><b>YOUR ID: </b> <input type="text" id="anid" value="%s" ></tr><br>
+          <tr><b>TITLE: </b> %s </tr> <br>
+          <br>
+      ''' % (taskid, docid, anid, fname)
     return text
 
 #print (template_head('1','2'))
 
 def template_body1(sentences):
     head = '''<tr>
-        <td width="200px">SENTENCE ID</td>
+        <td width="400px">SENTENCE ID</td>
     '''
     idx = 0
     fulltext = ''
@@ -44,7 +48,7 @@ def template_body1(sentences):
 def template_body2(sentences):
     fulltext = ''
     for i in range(len(sentences)): #+1
-        a = '<td>%s. %s</td>' % (i+1, sentences[i])
+        a = '<td>%s.) %s</td>' % (i+1, sentences[i])
         
         bid = 1
         btext = ''
@@ -54,7 +58,7 @@ def template_body2(sentences):
                 b = '<td></td>\n'
             else:
                 b = '''
-                <td> 
+                <td> <div><span title="%s">(%s)</span> <span title="%s">(%s)</span></div>
                     <select id="s_%s_%s">
                     <option value="5" selected="selected">none</option>
                     <option value="1">sequence_of</option>
@@ -63,7 +67,7 @@ def template_body2(sentences):
                     <option value="4">detail_of</option>
                     </select>
                 </td> 
-                ''' % (i+1, bid)
+                ''' % (sentences[i],i+1, sentences[bid-1],bid, i+1, bid)
             bid +=1
             btext = btext+' '+b
 
@@ -182,15 +186,44 @@ def template_js(taskid, anid, docid, sentences):
 
     return head+body+foot
 
-text = "This is sentence one. This is sentence two. This is sentence three. This is sentence four. This is sentence five. This is sentence six."
-#text = "This is sentence one. This is sentence two. This is sentence three."
+# text = "This is sentence one. This is sentence two. This is sentence three. This is sentence four. This is sentence five. This is sentence six."
+# #text = "This is sentence one. This is sentence two. This is sentence three."
 
-taskid, anid, docid = '2', 'your name here', 'sample'
-fname = docid+'_Task_'+taskid+'.html'
-sentences = split_sentences(text)
-# print (js_func(sentences))
-# import sys
-# sys.exit()
-template = template_head(taskid, docid, anid) + '\n' + template_body1(sentences) + '\n' + template_body2(sentences) + '\n' + template_js(taskid, anid, docid, sentences)
-with open(fname, 'w') as fw:
-    fw.write(template)
+# taskid, anid, docid = '2', 'your name here', 'sample'
+# fname = 'results/'+docid+'_Task_'+taskid+'.html'
+# sentences = split_sentences(text)
+# txt = '2012up/高流動性・高延性ポリプロピレンの構造と物性 - Copy.txt'
+# #sentences = cleantext(txt)
+
+# template = template_head(taskid, docid, anid) + '\n' + template_body1(sentences) + '\n' + template_body2(sentences) + '\n' + template_js(taskid, anid, docid, sentences)
+# with open(fname, 'w') as fw:
+#     fw.write(template)
+
+def read():
+    text = "This is sentence one. This is sentence two. This is sentence three. This is sentence four. This is sentence five."
+    taskid, anid, docid, fname = '2', 'your name here', 'sample', 'The title of the document'
+    saveresult = 'results/'+docid+'_Task'+taskid+'.html'
+    sentences = split_sentences(text)
+    template = template_head(taskid, docid, anid, fname) + '\n' + template_body1(sentences) + '\n' + template_body2(sentences) + '\n' + template_js(taskid, anid, docid, sentences)
+    with open(saveresult, 'w') as fw:
+        fw.write(template)
+    print (docid, len(sentences))
+
+def read_all(flder):
+    alltxt = glob.glob(flder+"/*.txt")
+    i = 87
+    for fl in alltxt:
+        fname = os.path.basename(fl).split('/')[0][:-4].replace(' ','')
+        docid = 'D00'+str(i)
+        sentences = cleantext(fl)
+        taskid, anid, docid = '2', 'your name here', docid
+        saveresult = 'results/2012up/'+docid+'_Task'+taskid+'.html'
+        
+        template = template_head(taskid, docid, anid, fname) + '\n' + template_body1(sentences) + '\n' + template_body2(sentences) + '\n' + template_js(taskid, anid, docid, sentences)
+        with open(saveresult, 'w') as fw:
+            fw.write(template)
+        print (i, docid, len(sentences))
+        i+=1
+
+#read_all("2012up")
+read()
